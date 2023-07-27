eba0ee4c1a2d910f1f1f373a6c3bc2308e841e4d
kushal261 <kush261999@gmail.com>
2023-07-27 21:20:15 +0530

    created Driver class for CalciImpl

--- Calci.java
+++ Calci.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public interface Calci
 {
 int getSum(int n1, int n2)
@@ -8,5 +9,19 @@ class CalciImpl implements Calci
 public int getSum(int n1, int n2)
 {
 return n1+n2;
+}
+}
+
+class class CalciDriver 
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+System.out.println("Enter two numbers to find their sum");
+int n1=s.nextInt();
+int n2=s.nextInt();
+CalciImpl c=new CalciImpl();
+int sum=c.getSum(n1,n2);
+System.out,println(n1+ "+" +n2+ "=" +sum);
 }
 }
